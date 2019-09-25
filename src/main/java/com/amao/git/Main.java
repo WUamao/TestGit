ce85a5a10de2774ffeb36eba98b4a0c4f83801ab
amao_glb <amao_glb@qq.com>
2019-09-25 20:47:02 +0800

    woshihhfffgggg

---
+++ src/main/java/com/amao/git/Main.java
@@ -0,0 +1,7 @@
+package com.amao.git;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("回家哈哈哈哈");
+	}
+}
---
+++ src/test/java/com/amao/git/TsetGit.java
@@ -0,0 +1,7 @@
+package com.amao.git;
+
+public class TsetGit {
+	public static void main(String[] args) {
+		System.out.println("我是打个");
+	}
+}
