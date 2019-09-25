fffa5b2f3c128b739398395a1fc9478351f974dd
amao_glb <amao_glb@qq.com>
2019-09-25 21:19:26 +0800

    wsfdsaf

--- src/main/java/com/amao/git/Main.java
+++ src/main/java/com/amao/git/Main.java
@@ -3,5 +3,10 @@ package com.amao.git;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("回家哈哈哈哈   update 11111");
+		System.out.println("回家哈哈哈哈   update 11111");
+		System.out.println("回家哈哈哈哈   update 11111");
+		System.out.println("回家哈哈哈哈   update 11111");
+		System.out.println("回家哈哈哈哈   update 11111");
+		
 	}
 }
